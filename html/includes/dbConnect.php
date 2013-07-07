4930c731f2e6795810d65040fd7d372bde7ba17f
mbl111 <matt@mbl111.com>
2013-07-07 21:03:13 +1000

    Changed the values to constants

--- html/includes/dbConnect.php
+++ html/includes/dbConnect.php
@@ -1,12 +1,13 @@
 <?php
-$host="localhost";
-$username="website";
-$password="T7PeY2cvd7s4seCZ";
-$db_name="quizsite";
+define("HOST", "localhost");
+define("DB_USERNAME", "website");
+define("DB_PASS", "T7PeY2cvd7s4seCZ");
+define("DB_NAME", "quizsite");
 //opens MYSQL connection
-mysql_connect("$host","$username","$password")or die("Cannot connect to server");
+mysql_connect(HOST,DB_USERNAME,DB_PASS)or die("Cannot connect to server");
 //select database
-mysql_select_db("$db_name")or die("Could not select DB");
+mysql_select_db(DB_NAME)or die("Could not select DB");
+
 
 function dbQuery($query){
     $result = mysql_query($query) or die(mysql_error());
