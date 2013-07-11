0a9708f84058d8b9a0c804328c9901361454d5e4
mbl111 <matt@mbl111.com>
2013-07-11 14:34:09 +1000

    Return user ID with validation

--- html/ajax/validateuser.php
+++ html/ajax/validateuser.php
@@ -8,7 +8,8 @@ if (isset($_POST['name']) && ! empty($_POST['name'])){
 	$exp = explode(" ", $name);
     $query = dbQuery("SELECT `id` FROM users WHERE `firstname`='{$exp[0]}' AND `lastname`='{$exp[1]}' LIMIT 1");
     if (mysql_num_rows($query) == 1){
-		echo "true";
+		$a = mysql_fetch_assoc($query);
+		echo "true {$a['id']}";
 	}else{
 		echo "No user found by this name";
 	}
