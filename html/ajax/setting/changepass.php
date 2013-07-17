8557adaa6b980c66d23776b7be90424f7f923ef5
mbl111 <matt@mbl111.com>
2013-07-17 23:33:41 +1000

    Checked that the passes match

--- html/ajax/setting/changepass.php
+++ html/ajax/setting/changepass.php
@@ -3,12 +3,17 @@ session_start();
 include("../../includes/include.php");
 include_once("../../includes/dbConnect.php");
 mustBeLoggedIn();
-if (isset($_POST['pass']) and isset($_POST['id'])){
+if (isset($_POST['pass']) and isset($_POST['pass2']) and isset($_POST['id'])){
 	$id = $_POST['id'];
 	if ($id == $_SESSION['userid'] || studentBelongsTo($_SESSION['userid'], $id)){
 		$first = md5($_POST['pass']);
-		$query = dbQuery("UPDATE users SET `password`='$first' WHERE id={$id}");
-		echo "true";
+		$second = md5($_POST['pass2']);
+		if ($first == $second){
+			$query = dbQuery("UPDATE users SET `password`='$first' WHERE id={$id}");
+			echo "true";
+		}else{
+			echo "Passwords did not match";
+		}
 	}else{
 		echo "Not your account or student!";
 	}
